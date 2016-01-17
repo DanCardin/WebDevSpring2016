2c36b8c872ab88a2fc3b7a8c564296dffb2fb0e9
DanCardin <ddcardin@gmail.com>
2016-01-17 09:43:25 -0500

    Start app shell

--- server/server.ts
+++ server/server.ts
@@ -4,11 +4,13 @@ let ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 let port: number = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 let app = express();
 
-app.use("/app", express.static(path.resolve(__dirname, "../client/app")));
+app.use("/app", express.static(path.resolve(__dirname, "./app")));
+app.use("/css", express.static(path.resolve(__dirname, "./css")));
+app.use("/public", express.static(path.resolve(__dirname, "./")));
 app.use("/node_modules", express.static(path.resolve(__dirname, "../node_modules")));
 
 let renderIndex = (req: express.Request, res: express.Response) => {
-    res.sendFile(path.resolve(__dirname, "../client/index.html"));
+    res.sendFile(path.resolve(__dirname, "./index.html"));
 };
 
 app.get("/*", renderIndex);
